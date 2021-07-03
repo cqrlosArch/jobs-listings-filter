5afa41b2ffaeb4c4870727506bd30d6f1f61d3b7
cqrlosArch <diaz.flores.c@gmail.com>
2021-07-03 21:45:20 +0200

    heading

--- src/components/List/List.styled.jsx
+++ src/components/List/List.styled.jsx
@@ -2,8 +2,6 @@ import styled from 'styled-components';
 
 const ListStyled = styled.section.attrs((props) => ({
   'aria-label': 'Listing',
-  role: 'heading',
-  'aria-level': 1,
 }))``;
 
 export default ListStyled;
