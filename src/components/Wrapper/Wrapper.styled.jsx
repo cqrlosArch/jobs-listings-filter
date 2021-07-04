824d7c4fd4509b13493995f30855a5b891121414
cqrlosArch <diaz.flores.c@gmail.com>
2021-07-04 20:00:35 +0200

    desktop

--- src/components/Wrapper/Wrapper.styled.jsx
+++ src/components/Wrapper/Wrapper.styled.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const WrapperStyled = styled.main`
   padding: 2em 2em;
   @media screen and (min-width: 768px) {
-    max-width: 950px;
+    max-width: 1000px;
     margin-left: auto;
     margin-right: auto;
   }
